33e602d564a2417bf88a655eade2acccaddd4bc2
matheusabreu <matheusabreu.dev@gmail.com>
2018-04-17 16:54:58 -0300

    feat(): open local pdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { File } from "@ionic-native/file";
+import { FileTransfer } from "@ionic-native/file-transfer";
+import { DocumentViewer } from "@ionic-native/document-viewer";
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +28,10 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    File,
+    FileTransfer,
+    DocumentViewer
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer';
+import { File } from '@ionic-native/file';
+import { FileTransfer } from '@ionic-native/file-transfer';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private platform: Platform,
+    private document: DocumentViewer,
+    private file: File,
+    private transfer: FileTransfer
+  ) {
 
+  }
+
+  /**
+   * Open Local Pdf (offline)
+   * 1. Get the dir path
+   * 2. Set options to Documents Viewer App
+   * 3. Open the pdf file
+   */
+  openLocalPdf() {
+    let path = null;
+
+    if (this.platform.is('ios')) {
+      path = this.file.documentsDirectory;
+    } else {
+      path = this.file.applicationDirectory;
+    }
+
+    const options: DocumentViewerOptions = {
+      title: 'PDF Title'
+    };
+
+    this.document.viewDocument(path + 'www/assets/pdf/typescript_tutorial.pdf', 'application/pdf', options);
   }
 
 }
